14afcc78ca034f434b85b06f9ed572727e30302e
akshay85131 <akshay85131@gmail.com>
2019-05-10 08:58:54 +0530

    done with delete

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import "./App.css";
+// import List from "./List";
+import Todos from "./components/Todos";
+import Header from "./components/layouts/Header";
+import TodoItems from "./components/TodoItems";
+import AddTodo from "./components/AddTodo";
+import uuid from 'uuid'
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: uuid.v4(), title: "complete all work on focus hours", completed: false },
+      { id: uuid.v4(), title: "take rest", completed: false }
+    ]
+  };
+
+addTodo = (title)=>{
+  const newTodo = {
+    id:uuid.v4(),
+    title,
+    completed:false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] })
+}
+
+
+
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
+
+  // console.log(this.state.todos)
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,20 @@
+// import React from "react";
+
+// const List = props => {
+//   return (
+//     <ul>
+//       {props.items.map((item, index) => (
+//         <li key={index}>
+//           {item}>
+//           <button type= 'button' className="deleteButton" 
+//           // onClick={this.props.deleteItem(item.key)}
+//           >
+//             Delete
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default List;
